0f2c4febab097281cfbbd5eb17c38de7d4ea0f46
mijikim <miji.kim926@gmail.com>
2014-08-26 16:21:00 -0600

    user can erase todos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,69 @@
 //= require jquery_ujs
 //= require_tree .
 
+
 $(document).ready(function() {
-  $('body').append("<h1>hello world, I'm from only javascript!</h1>");
+
+  var body = $("body");
+
+  body.attr('style', 'text-align: center');
+
+  var formHtml = "";
+
+  formHtml += "<h1>Todoly</h1>";
+  formHtml += "<form>";
+  formHtml += "<input type='text' name='todo' id='todo' />";
+  formHtml += "<br>";
+  formHtml += "<input type='submit' value='Create Todo' id='addtodo'/>";
+  formHtml += "</form>";
+
+  var todosHtml = "";
+
+  todosHtml += "<h2>Todo!</h2>";
+  todosHtml += "<div id='flashmsgs'></div>"
+  todosHtml += "<ul>";
+  todosHtml += "<li></li>";
+  todosHtml += "</ul>";
+
+  body.append(formHtml + todosHtml);
+
+  $("#addtodo").on("click", function (e) {
+    e.preventDefault();
+    var flashmsg = "<div id='flashmsg'>Todo Created <span class='closeflashmsg'>x</span></div>";
+    var todo = $("#todo").val();
+    $("#flashmsgs").append(flashmsg);
+    body.append("<li>" + todo + "<span class='closetodos'>x</span></li>");
+    $("#flashmsg").fadeOut(8000);
+    setTimeout(function () {
+      $("#flashmsg").remove();
+    }, 8000);
+
+    $(".closeflashmsg").on("click", function () {
+      $(this).parent("#flashmsg").remove()
+    });
+
+    $(".closetodos").on("click", function () {
+      $(this).parent("li").remove()
+    });
+
+
+
+
+  });
+
+//  var body = $("body");
+
+//  alert(body.length);
+
+//  body.append("<a href='/hello' id='Miji'>Mi Ji</a>");
+
+//  var linkClick = function(event) {
+//    $(event.target).parents('li');
+//    event.preventDefault();
+//    alert("hello")
+//  }
+//
+//  $("a").on("click", linkClick);
+
 });
+
